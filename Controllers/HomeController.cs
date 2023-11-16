68c59221385c5a11d9d00b618a5021c0d0aa7c11
Kenan02 <kenano321@hotmail.com>
2023-11-16 08:35:37 +0100

    push

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Generic_Employee_Dashboard.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AddUsers()
+        {
+            return View();  
+        }
+
+
+        public IActionResult AddUsers(UserModel user)
+        {
+            return View();
+        }
+    }
+}
---
+++ DemoContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Generic_Employee_Dashboard
+{
+    public class DemoContext : DbContext
+    {
+        public DbSet<UserModel> Users { get; set; }
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseSqlite(@"Data Source=C:\temps\demo.db");
+       
+    }
+}
